e3e1304fe052e3a3f9a5e058be9a68528fc1725c
Martha30 <53914325+Martha30@users.noreply.github.com>
2020-04-26 19:48:42 -0600

    Initial commit

---
+++ app/src/main/java/com/example/lab4/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.lab4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            @Suppress("UNUSED_VARIABLE")
+            val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main
+    }
+}
+
+    class DataBindingUtil {
+
+    }
